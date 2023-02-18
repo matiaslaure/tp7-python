386bf014f27ff64e7a619a2024cf04faad786a6f
Matias Andres Garcia Laure <garcialaurematias93rgmail.com>
2023-02-18 07:59:21 -0300

    TP7 python

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
---
+++ tp7.py
@@ -0,0 +1,8 @@
+import calculadora
+
+print(calculadora.division(8, 4))
+print(calculadora.multiplicar(5, 9))
+print(calculadora.resta(12, 5))
+print(calculadora.suma(15, 3))
+
+
